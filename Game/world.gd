7c61129970b860d859b62fad2b01b6b303eb01c9
hexy <hexagonhexagon163@gmail.com>
2025-08-07 15:18:02 -0400

    synchronized player boards now works
    resolved issue #59

--- Game/world.gd
+++ Game/world.gd
@@ -3,7 +3,38 @@ extends Control
 @export var BoardHolder : Node
 @export var PlayerBoard : PackedScene
 
+var num_players_ready := 0
+
 @onready var _GameState := %GameState
+
+func add_player_board(player_id: int, player_name: String) -> void:
+	var board = PlayerBoard.instantiate()
+	board.OwnerId = player_id
+	board.OwnerName = player_name
+	BoardHolder.add_child(board)
+
+@rpc("call_local", "reliable")
+func set_up_game(world_seed: int) -> void:
+	seed(world_seed)
+
+	var players = ConnectionSystem.players.duplicate()
+	players[multiplayer.get_unique_id()] = Globals.player_name
+
+	var sorted_player_ids = players.keys()
+	sorted_player_ids.sort()
+
+	for player_id in sorted_player_ids:
+		var player_name = players[player_id]
+		add_player_board(player_id, player_name)
+
+@rpc("any_peer", "call_local", "reliable")
+func register_ready() -> void:
+	assert(multiplayer.is_server())
+	num_players_ready += 1
+	var total_num_players = ConnectionSystem.players.size() + 1
+	
+	if num_players_ready >= total_num_players:
+		set_up_game.rpc(randi())
 
 func _ready() -> void:
 	print(
@@ -12,15 +43,10 @@ func _ready() -> void:
 			Globals.player_name,
 			ConnectionSystem.players,
 	])
-	var your_board := PlayerBoard.instantiate()
-	print("instantiated your_board")
-	your_board.OwnerId = multiplayer.get_unique_id()
-	your_board.OwnerName = Globals.player_name
-	BoardHolder.add_child(your_board)
-	print("added board to tree")
-	
 	if multiplayer.is_server():
 		_GameState.example_int = 42
+
+	register_ready.rpc_id(1)
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
--- Systems/connection.gd
+++ Systems/connection.gd
@@ -87,7 +87,7 @@ func _player_connected(id: int) -> void:
 	
 	
 ## Registers a player to the server
-@rpc("any_peer")
+@rpc("any_peer", "call_local")
 func register_player(new_player_name: String) -> void:
 	print("Received remote register_player")
 	var id : int = multiplayer.get_remote_sender_id()
