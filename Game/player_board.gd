cd1cdbecd3b404950272bd6f329dfe9a396624b0
hexy <hexagonhexagon163@gmail.com>
2025-08-07 16:16:21 -0400

    refactor player board to use %, extend control

--- Game/player_board.gd
+++ Game/player_board.gd
@@ -1,7 +1,13 @@
-extends VBoxContainer
+extends Control
 
-@export var MineMap : TileMapLayer
-@export var FactoryMap : TileMapLayer
+@onready var _VerticalListContainer := %VerticalListContainer
+@onready var _Sky := %Sky
+@onready var _FactoryFloor := %FactoryFloor
+@onready var _Mine := %Mine
+@onready var _PlayerNameLabel := %PlayerNameLabel
+@onready var _MineTiles := %MineTiles
+@onready var _FactoryTiles := %FactoryTiles
+
 
 # multiplayer properties
 @export var OwnerId : int
@@ -22,15 +28,16 @@ func _ready() -> void:
 	var layer_height_px := tile_size * LayerThickness
 
 	custom_minimum_size = Vector2i(board_width_px, 0)
-	$Sky.custom_minimum_size = Vector2i(0, SkyHeight)
-	$FactoryFloor.custom_minimum_size = Vector2i(0, layer_height_px)
-	$Mine.custom_minimum_size = Vector2i(0, layer_height_px)
-	%PlayerNameLabel.text = "%s\n(%s)" % [OwnerName, OwnerId]
+	_VerticalListContainer.custom_minimum_size = Vector2i(board_width_px, 0)
+	_Sky.custom_minimum_size = Vector2i(0, SkyHeight)
+	_FactoryFloor.custom_minimum_size = Vector2i(0, layer_height_px)
+	_Mine.custom_minimum_size = Vector2i(0, layer_height_px)
+	_PlayerNameLabel.text = "%s\n(%s)" % [OwnerName, OwnerId]
 
 	for x in range(NumCols):
 		for y in range(LayerThickness):
 			var tileCoords := Vector2i(x, y)
 			var randomOreId=randi_range(1,5)
 			
-			MineMap.set_cell(tileCoords, 0, Vector2i(randomOreId,0))
-			FactoryMap.set_cell(tileCoords, 0, Vector2i(0,0))
+			_MineTiles.set_cell(tileCoords, 0, Vector2i(randomOreId,0))
+			_FactoryTiles.set_cell(tileCoords, 0, Vector2i(0,0))
