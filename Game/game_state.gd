b2188ca15825dc72c7dddf6f2b3328031a1bd52d
Robin Genetti <robin@pondwatch.net>
2025-08-07 14:33:38 -0400

    Closes Issue #71, implements server authoritative game state.

---
+++ Game/game_state.gd
@@ -0,0 +1,3 @@
+extends Node
+
+@export var example_int: int
--- Game/world.gd
+++ Game/world.gd
@@ -3,6 +3,8 @@ extends Control
 @export var BoardHolder : Node
 @export var PlayerBoard : PackedScene
 
+@onready var _GameState := %GameState
+
 func _ready() -> void:
 	print(
 		"%s (%s) says the players are %s" % [
@@ -16,3 +18,12 @@ func _ready() -> void:
 	your_board.OwnerName = Globals.player_name
 	BoardHolder.add_child(your_board)
 	print("added board to tree")
+	
+	if multiplayer.is_server():
+		_GameState.example_int = 42
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		print("My game state is " + str(_GameState.example_int))
+		if multiplayer.is_server():
+			_GameState.example_int += 1
