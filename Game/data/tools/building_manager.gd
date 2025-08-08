9ea1c756014f04980dfe97cfebf607b9f300181b
Robin Genetti <robin@pondwatch.net>
2025-08-08 01:59:42 -0400

    Added images to solar panel and miner

--- Game/data/tools/building_manager.gd
+++ Game/data/tools/building_manager.gd
@@ -14,6 +14,10 @@ var _building_paths = {}
 @onready var _FactoryTile := %FactoryTile
 @onready var _EnergyDrain := %EnergyDrain
 @onready var _MoneyCost := %MoneyCost
+@onready var _FilenamePopup := %FilenamePopup
+@onready var _NewBuildingName := %NewBuildingName
+
+var _currently_selected_building: BuildingResource
 
 func _ready() -> void:
 	load_building_resources()
@@ -30,11 +34,45 @@ func load_building_resources() -> void:
 func _on_building_list_item_selected(index: int) -> void:
 	var path = _building_paths[index]
 	var building:BuildingResource = load(path)
-	show_building(building)
+	show_building(_currently_selected_building)
 	
 	
 func show_building(building:BuildingResource):
+	_currently_selected_building = building
+	
 	_UniqueId.text = str(building.unique_id)
 	_BuildingName.text = building.name
 	_EnergyDrain.text = str(building.energy_drain)
 	_MoneyCost.text = str(building.money_cost)
+	
+	_StoreIcon.texture = building.shop_icon
+	_FactoryTile.texture = building.factory_tile
+		
+func save_edits() -> void:
+	var current_index:int = _BuildingList.get_selected_items()[0]
+	var path = _building_paths[current_index]
+	ResourceSaver.save(_currently_selected_building,  "%s%s" % [building_resource_path, path])		
+		
+func create_new_building(filename: String) -> void:
+	var building: BuildingResource = BuildingResource.new()
+	building.unique_id = ResourceUID.create_id()
+	ResourceSaver.save(building,  "%s%s.tres" % [building_resource_path, filename])
+	show_building(building)
+
+
+func _on_new_button_pressed() -> void:
+	_FilenamePopup.show()
+
+
+func _on_ok_button_pressed() -> void:
+	create_new_building(_NewBuildingName.text)
+	_FilenamePopup.hide()
+
+
+func _on_cancel_filename_pressed() -> void:
+	_FilenamePopup.hide()
+
+
+func _on_new_building_name_text_submitted(new_text: String) -> void:	
+	create_new_building(_NewBuildingName.text)
+	_FilenamePopup.hide()
