d3ec761a8a4647d48bca74cddd4d79736efa7606
hexy <hexagonhexagon163@gmail.com>
2025-08-18 21:56:11 -0400

    Items global exports typed dictionary instead of typed list

--- Game/data/item_resource.gd
+++ Game/data/item_resource.gd
@@ -2,7 +2,6 @@ extends Resource
 
 class_name ItemResource
 
-@export var type : Item.Type
 @export var icon : Texture2D
 @export var can_trade := true
 @export var sell_value := 0
--- Game/items.gd
+++ Game/items.gd
@@ -1,15 +1,6 @@
 extends Node
 
-@export var items_list: Array[ItemResource]
-
-var items_dict: Dictionary
+@export var items_dict: Dictionary[Item.Type, ItemResource]
 
 func get_info(type: Item.Type) -> ItemResource:
 	return items_dict[type]
-
-func _ready() -> void:
-	for type in Item.Type.values():
-		for item in items_list:
-			if type == item.type:
-				items_dict[type] = item
-				break # break inner loop, outer loop keeps going
