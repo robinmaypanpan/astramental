996542b4da00d5b0087b603cf7dfaf64c1267608
hexy <hexagonhexagon163@gmail.com>
2025-08-20 18:53:59 -0400

    add data structure for ore generation to Ores

---
+++ Game/data/layer_generation_resource.gd
@@ -0,0 +1,4 @@
+class_name LayerGenerationResource extends Resource
+
+@export var background_rock: Ore.Type
+@export var ores: Array[OreGenerationResource]
--- Game/ores.gd
+++ Game/ores.gd
@@ -2,6 +2,13 @@ extends Node
 
 @export var ores_tileset: TileSet
 @export var ores_dict: Dictionary[Ore.Type, OreResource]
+@export var ores_generation: Array[LayerGenerationResource]
 
-func get_info(type: Ore.Type) -> OreResource:
-	return ores_dict[type]
+func get_atlas_coordinates(type: Ore.Type) -> Vector2i:
+	return ores_dict[type].atlas_coordinates
+
+func get_yield(type: Ore.Type) -> Item.Type:
+	return ores_dict[type].item_yield
+
+func get_layer_generation_data(layer_num: int) -> LayerGenerationResource:
+	return ores_generation[layer_num]
