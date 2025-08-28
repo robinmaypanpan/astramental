7d797cc72007ba2f1a58d7c9c951e45af0be2ec9
hexy <hexagonhexagon163@gmail.com>
2025-08-28 19:02:41 -0400

    document resources

--- Game/data/item_resource.gd
+++ Game/data/item_resource.gd
@@ -3,5 +3,7 @@ extends Resource
 class_name ItemResource
 
 @export var icon : Texture2D
+## Whether the resource can be traded or not.
 @export var can_trade := true
+## The money you get for selling one unit of the resource.
 @export var sell_value := 0
