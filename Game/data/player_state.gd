1335bec919c96f4897290be3eb29ec9f3af7853d
hexy <hexagonhexagon163@gmail.com>
2025-08-28 19:02:48 -0400

    document player states

--- Game/data/player_state.gd
+++ Game/data/player_state.gd
@@ -1,5 +1,8 @@
 class_name PlayerState extends Node
 
+## The player id, assigned by the multiplayer controller.
 @export var id:int
+## The player index, which starts at 1 for the server and increases by 1 for each successive player.
 @export var index:int
+## The amount of each item that this player currently has.
 @export var items: Dictionary[Item.Type, float]
