75d85f808242719928faaad208b5895f8d752e7d
hexy <hexagonhexagon163@gmail.com>
2025-09-10 17:45:43 -0400

    Asteroid stores reference to Model

--- Game/asteroid.gd
+++ Game/asteroid.gd
@@ -2,6 +2,8 @@ class_name Asteroid extends Control
 
 @onready var _BoardHolder := %BoardHolder
 
+@export var _Model: GameModel
+
 func get_player_boards() -> Array[Node]:
 	return _BoardHolder.get_children()
 	
