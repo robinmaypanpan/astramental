be210f666fb73e4a6184e4a8f2038088e4cf5e91
hexy <hexagonhexagon163@gmail.com>
2025-09-11 02:27:51 -0400

    adjust PlayerBoard to have correct height for all layers

--- Game/player_board.gd
+++ Game/player_board.gd
@@ -2,9 +2,9 @@ extends Control
 
 @onready var _VerticalListContainer := %VerticalListContainer
 @onready var _Sky := %Sky
-@onready var _FactoryFloor := %FactoryFloor
+@onready var _FactoryAndMine := %FactoryAndMine
 @onready var _PlayerNameLabel := %PlayerNameLabel
-@onready var _FactoryTiles: BuildingTileMap = %FactoryTiles
+@onready var _FactoryAndMineTiles: BuildingTileMap = %FactoryAndMineTiles
 
 # multiplayer properties
 var owner_id : int
@@ -24,13 +24,15 @@ func _ready() -> void:
 		ConnectionSystem.host_server()
 		
 	player = ConnectionSystem.get_player(owner_id)
-	building_tile_maps = [_FactoryTiles]
+	building_tile_maps = [_FactoryAndMineTiles]
 		
 	print("doing ready for %s (%s)" % [player.name, owner_id])
 
 	var tile_size := 16 * TileMapScale
 	var board_width_px := tile_size * NumCols
 	var layer_height_px := tile_size * LayerThickness
+	# 1 factory layer + x mine layers
+	var num_layers := Ores.get_num_mine_layers() + 1
 
 	custom_minimum_size = Vector2i(board_width_px, 0)
 	_VerticalListContainer.custom_minimum_size = Vector2i(board_width_px, 0)
@@ -38,12 +40,15 @@ func _ready() -> void:
 	_Sky.custom_minimum_size = Vector2i(0, SkyHeight)
 	_PlayerNameLabel.text = "%s\n(%s)" % [player.name, player.index]
 
-	_FactoryFloor.custom_minimum_size = Vector2i(0, layer_height_px)
+	_FactoryAndMine.custom_minimum_size = Vector2i(0, layer_height_px * num_layers)
+	_FactoryAndMineTiles.tile_map_scale = TileMapScale
+	_FactoryAndMineTiles.layer_thickness = LayerThickness
 
 	# Set up factory tiles to be all white tiles
+	var white_tile_atlas_coordinates = Vector2i(0, 0)
 	for x in range(NumCols):
 		for y in range(LayerThickness):
-			_FactoryTiles.set_background_tile(x, y, Vector2i(0, 0))
+			_FactoryAndMineTiles.set_background_tile(x, y, white_tile_atlas_coordinates)
 
 ## Given an instantiated mine layer, add it as a child to this board.
 func add_mine_layer(mine_layer: Node) -> void:
