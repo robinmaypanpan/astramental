8c792a0087835a826021b05090f693da5abe0025
Robin Genetti <robin@pondwatch.net>
2025-09-16 01:03:51 -0400

    Fix game crash when loading

--- Game/ui/main ui/left_panel.gd
+++ Game/ui/main ui/left_panel.gd
@@ -4,14 +4,6 @@ class_name LeftPanel extends Control
 @onready var debug_menu_panel := %Debug
 @onready var build_menu := %Build
 
-
-func _ready() -> void:
-	Model.game_ready.connect(_on_game_ready)
-
 ## Returns the build menu
 func get_build_menu() -> BuildMenu:
 	return build_menu
-
-## Setup the left panel at the start of a game
-func _on_game_ready() -> void:
-	resource_display.setup_game()
