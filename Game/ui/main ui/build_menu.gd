c7c20a5def3ef648e2042f0e04d3806947795ab1
hexy <hexagonhexagon163@gmail.com>
2025-10-03 16:21:14 -0400

    build menu options are disabled if you can't afford them

--- Game/ui/main ui/build_menu.gd
+++ Game/ui/main ui/build_menu.gd
@@ -18,9 +18,19 @@ func _ready() -> void:
 		building_list.set_item_text(index, building.name)
 		_buildings.push_back(building.id)
 
+	Model.item_count_changed.connect(_on_item_count_changed)
+
 
 func _on_building_list_item_clicked(
 	index: int, _at_position: Vector2, _mouse_button_index: int
 ) -> void:
 	var building: String = _buildings[index]
 	on_building_clicked.emit(building)
+
+func _on_item_count_changed(_player_id: int, _type: Types.Item, _new_count: float) -> void:
+	_disable_unaffordable_buildings()
+
+func _disable_unaffordable_buildings() -> void:
+	for index in range(_buildings.size()):
+		var building_id: String = _buildings[index]
+		building_list.set_item_disabled(index, not Model.can_build(building_id))
