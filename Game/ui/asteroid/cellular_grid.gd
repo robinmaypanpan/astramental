8aeeb9f17d87a5042c36c95bd4f7ca79ea4e0bf6
Robin Genetti <robin@pondwatch.net>
2025-10-03 15:01:12 -0400

    Added comments and other cleanup based on feedback

--- Game/ui/asteroid/cellular_grid.gd
+++ Game/ui/asteroid/cellular_grid.gd
@@ -9,19 +9,19 @@ var _actual_rows: int = 5
 var _actual_cols: int = 5
 
 @onready var _grid_container: GridContainer = %GridContainer
-	
+
 ## Sets the size of this grid to the indicated number of rows/cols
 func generate_grid(rows:int, cols:int):
 	_actual_rows = rows
 	_actual_cols = cols
-	
+
 	_grid_container.columns = cols
-	
+
 	## Clear the existing container first
 	for child:Control in _grid_container.get_children():
 		_grid_container.remove_child(child)
 		child.queue_free()
-	
+
 	for r: int in range(rows):
 		for c: int in range(cols):
 			var cell = cell_scene.instantiate()
@@ -31,17 +31,17 @@ func generate_grid(rows:int, cols:int):
 ## Returns an iterator over all cells
 func all_cells() -> AllCellIterator:
 	return AllCellIterator.new(self, _actual_rows, _actual_cols)
-	
-	
+
+
 ## Returns the cell at the indicated row and column
 func get_cell(row:int, col:int) -> Control:
-	var child_index = col + row * _actual_cols	
+	var child_index = col + row * _actual_cols
 	var child:Control = _grid_container.get_child(child_index)
 	return child
-	
-	
+
+
 ## Returns the cell under the provided point
-func get_cell_under_local_point(point:Vector2i) -> Vector2i:
+func get_cell_coordinates_at_local_point(point:Vector2i) -> Vector2i:
 	var my_rect := get_rect()
 	if not my_rect.has_point(point):
 		return Vector2i(-1,-1)
@@ -50,12 +50,21 @@ func get_cell_under_local_point(point:Vector2i) -> Vector2i:
 	var result := Vector2i(point.x / cell_width, point.y / cell_height)
 	return result
 
-
+## This is an iterator that will iterate over all cells in this cellular grid.
 class AllCellIterator:
-	var grid: CellularGrid
+	## Stores a reference to the grid we're iterating over.
+	var grid: CellularGrid # TODO RPG: Is this actually Pass by reference??
+
+	## Store the current row in the iterator
 	var row := 0
+
+	## Store the current column in the iterator
 	var col := 0
+
+	## Cache the number of rows in this grid
 	var rows := 0
+
+	## Cache the number of columns in this grid
 	var cols := 0
 
 
@@ -65,17 +74,22 @@ class AllCellIterator:
 		self.cols = cols
 
 
+	## Using the example from the docs on creating your own iterators, this function will
+	## determine if this iterator is done running or not. It is technically private and not
+	## used by anything
 	func should_continue():
 		return row < rows and cols > 0
 
-
-	func _iter_init(arg):
+	## When this iterator starts running, this will initialize our row/col values.
+	func _iter_init(_arg):
 		row = 0
 		col = 0
 		return should_continue()
 
 
-	func _iter_next(arg):
+	## When this iterator looks for the next item in the list, it calls this function to
+	## update the position of iteration and determine if we are done or not.
+	func _iter_next(_arg):
 		col += 1
 		if col >= cols:
 			# Go to the next row
@@ -84,5 +98,6 @@ class AllCellIterator:
 		return should_continue()
 
 
-	func _iter_get(arg):
+	## When the iterator goes to get any item in the list, this is how it gets it.
+	func _iter_get(_arg):
 		return grid.get_cell(row, col)
