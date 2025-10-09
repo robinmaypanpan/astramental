a7359bf4ca65b3874fc10e3faf3bce4b5494be04
Robin Genetti <robin@pondwatch.net>
2025-10-09 10:53:21 -0400

    Added mouse hover events to build menu items

--- Game/ui/main ui/build_menu_item.gd
+++ Game/ui/main ui/build_menu_item.gd
@@ -11,6 +11,12 @@ extends Control
 ## The color to apply when this menu item is disabled
 @export var disabled_modulation: Color
 
+## Background color when hovered
+@export var hovered_self_modulation: Color
+
+## Background color when not hovered
+@export var unhovered_self_modulation: Color
+
 @onready var ready_light: ColorRect = %ReadyLight
 @onready var icon: TextureRect = %Icon
 @onready var building_name: Label = %Name
@@ -27,6 +33,7 @@ func set_building_resource(building: BuildingResource):
 		cost_container.add_child(new_item_cost)
 		new_item_cost.set_item_cost(item_cost)
 		
+		
 func set_enabled(new_enabled: bool) -> void:
 	if new_enabled:
 		ready_light.color = Color.GREEN
@@ -35,7 +42,16 @@ func set_enabled(new_enabled: bool) -> void:
 		ready_light.color = Color.RED
 		modulate = disabled_modulation
 		
+		
 func clear_cost_container() -> void:
 	for child in cost_container.get_children():
 		cost_container.remove_child(child)
 		child.queue_free()
+
+
+func _on_mouse_entered() -> void:
+	self_modulate = hovered_self_modulation
+
+
+func _on_mouse_exited() -> void:
+	self_modulate = unhovered_self_modulation
