9b2cb6140bacf1d8764533f932b903af17fd1ef4
hexy <hexagonhexagon163@gmail.com>
2025-10-18 15:07:54 -0400

    move storage cap logic to settings

--- Game/data/scripts/settings_resource.gd
+++ Game/data/scripts/settings_resource.gd
@@ -1,11 +1,27 @@
 class_name SettingsResource
 extends Resource
 
+## The hard cap for resources in the game.
+const STORAGE_LIMIT_HARD_CAP: float = 1_000_000_000
+
 ## Defines the resources people start the game with.
 @export var starting_resources: Dictionary[Types.Item, float]
 
-## Provides base limits to storage for each item.
-@export var storage_limits: Dictionary[Types.Item, float]
+## Provides base caps to storage for each item.
+@export var storage_caps: Dictionary[Types.Item, float]
 
 ## Define the update interval for the game, defined as how many seconds equals 1 tick.
 @export_range(0.05, 1) var update_interval: float
+
+
+## Get the storage limits for all items. Return the storage limit hard cap if storage_caps
+## doesn't include that item.
+func get_storage_caps() -> Dictionary[Types.Item, float]:
+    var new_storage_caps = storage_caps.duplicate()
+    for item in Types.Item.values():
+        new_storage_caps.get_or_add(item, STORAGE_LIMIT_HARD_CAP)
+    return new_storage_caps
+
+## Get the storage limit for the specified item.
+func get_storage_cap_item(item: Types.Item) -> float:
+    return storage_caps.get(item, STORAGE_LIMIT_HARD_CAP)
