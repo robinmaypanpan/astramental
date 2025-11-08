c146eafd4442671f30cddd61ee622f99baedd23d
hexy <hexagonhexagon163@gmail.com>
2025-11-08 13:01:15 -0500

    add RPCs for trade route modification

--- Game/data/other_systems/trade_system.gd
+++ Game/data/other_systems/trade_system.gd
@@ -12,22 +12,13 @@ var _next_trade_route_id: int = 0
 ## Add a new trade route.
 func add_trade_route(
 	sending_player_id: int, receiving_player_id: int, item: Types.Item, amount: float
-) -> int:
-	var new_trade_route: TradeRoute = TradeRoute.new(
-		_next_trade_route_id, sending_player_id, receiving_player_id, item, amount
-	)
-	trade_routes.append(new_trade_route)
-	_next_trade_route_id += 1
-	return new_trade_route.id
+) -> void:
+	_broadcast_add_trade_route.rpc(sending_player_id, receiving_player_id, item, amount)
 
 
 ## Remove an existing trade route by its internal id.
 func remove_trade_route(trade_route_id: int) -> void:
-	var index_to_remove: int = trade_routes.find_custom(
-		func(elem): return elem.id == trade_route_id
-	)
-	if index_to_remove != -1:
-		trade_routes.remove_at(index_to_remove)
+	_broadcast_remove_trade_route.rpc(trade_route_id)
 
 
 ## Find all trade routes where the receiving player is the given player id.
@@ -69,3 +60,25 @@ func update() -> void:
 			Model.increase_item_change_rate(sending_player_id, item, -send_amount_per_sec)
 			Model.increase_item_count(receiving_player_id, item, send_amount_per_tick)
 			Model.increase_item_change_rate(receiving_player_id, item, send_amount_per_sec)
+
+
+## Add a new trade route for all players.
+@rpc("any_peer", "call_local", "reliable")
+func _broadcast_add_trade_route(
+	sending_player_id: int, receiving_player_id: int, item: Types.Item, amount: float
+) -> void:
+	var new_trade_route: TradeRoute = TradeRoute.new(
+		_next_trade_route_id, sending_player_id, receiving_player_id, item, amount
+	)
+	trade_routes.append(new_trade_route)
+	_next_trade_route_id += 1
+
+
+## Remove an existing trade route by its internal id for all players.
+@rpc("any_peer", "call_local", "reliable")
+func _broadcast_remove_trade_route(trade_route_id: int) -> void:
+	var index_to_remove: int = trade_routes.find_custom(
+		func(elem): return elem.id == trade_route_id
+	)
+	if index_to_remove != -1:
+		trade_routes.remove_at(index_to_remove)
