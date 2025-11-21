da0ad1e93fa9101441672d0108186df4eff93a68
hexy <hexagonhexagon163@gmail.com>
2025-11-21 13:13:05 -0500

    temp progress

---
+++ Game/model/player_state/building_model.gd
@@ -0,0 +1,17 @@
+class_name BuildingModel
+extends Node
+##
+
+##
+@export var buildings_serialized: Array
+
+##
+var buildings: Array[BuildingEntity]
+
+##
+var _buildings_shadow: Array[BuildingEntity]
+
+##
+var building_components: ComponentManager
+
+
