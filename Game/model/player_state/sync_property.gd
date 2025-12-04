06ff5db2c61f1802bddf01dfa2e789dbf5cdde11
hexy <hexagonhexagon163@gmail.com>
2025-12-04 00:48:35 -0500

    SyncProperty value_server converted to PackedByteArray so it can be synchronized
    Use bytes_to_var and var_to_bytes

--- Game/model/player_state/sync_property.gd
+++ Game/model/player_state/sync_property.gd
@@ -1,10 +1,10 @@
 class_name SyncProperty
 extends Node
 
-## Value reduced to primitives that can be sent across the network with MultiplayerSynchronizer.
+## Value reduced to PackedByteArray that can be sent across the network by MultiplayerSynchronizer.
 ## Clients read this value to get the value they need.
 ## The server writes to this value to publish properties across the network.
-@export var value_server: Variant
+@export var value_server: PackedByteArray
 
 ## The most up-to-date version of the value known.
 ## The server uses this as its working copy of the value as changes are being made to it.
@@ -14,30 +14,24 @@ var value_client: Variant
 ## Serialize the client value so that it can be transferred across the network.
 ## Default behavior is to copy the value and return that, effectively not changing it.
 ## Can be re-implemented by subclasses.
-func serialize(value: Variant) -> Variant:
-    if value is Array or value is Dictionary:
-        return value.duplicate()
-    else:
-        return value
+func serialize(value: Variant) -> PackedByteArray:
+	return var_to_bytes(value)
 
 
 ## Deserialize the server value so that it can be turned into a usable client value.
 ## Default behavior is to copy the value and return that, effectively not changing it.
 ## Can be re-implemented by subclasses.
-func deserialize(value: Variant) -> Variant:
-    if value is Array or value is Dictionary:
-        return value.duplicate()
-    else:
-        return value
+func deserialize(value: PackedByteArray) -> Variant:
+	return bytes_to_var(value)
 
 
 ## Publish the value to the network by copying the client value to the server value.
 func publish() -> void:
-    assert(multiplayer.is_server())
-    value_server = serialize(value_client)
+	assert(multiplayer.is_server())
+	value_server = serialize(value_client)
 
 
 ## Sync the value from the network to the client by copying the server value to the client value.
 func sync() -> void:
-    assert(not multiplayer.is_server())
-    value_client = deserialize(value_server)
+	assert(not multiplayer.is_server())
+	value_client = deserialize(value_server)
